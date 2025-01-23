7239835fe08bd1b2fcfec5a528064b53e89238a6
DEOKJAE KWON <juintin@kakao.com>
2025-01-23 23:06:17 +0900

    feat: add inputName logic

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,8 +1,18 @@
+import java.io.*;
+
 public class TaskManager {
 
     private static TaskManager taskManager;
 
+    private final String INPUT_ERROR_MESSAGE = "Error has occurred. Please enter it again.";
+    private final String NAME_ERROR_MESSAGE = "Name cannot be empty. Please enter it again.";
+
+    private String name;
+    private BufferedReader br;
+
     private TaskManager() {
+        name = "";
+        br = new BufferedReader(new InputStreamReader(System.in));
     }
 
     public static TaskManager getInstance() {
@@ -13,7 +23,29 @@ public class TaskManager {
     }
 
     public void start() {
-        System.out.println("Task manager started!");
+        inputName();
+    }
+
+    private void inputName() {
+        while (true) {
+            System.out.print("Please enter your name: ");
+            try {
+                String name = br.readLine();
+                validateName(name);
+                this.name = name;
+                break;
+            } catch (IOException e) {
+                System.out.println(INPUT_ERROR_MESSAGE);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void validateName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException(NAME_ERROR_MESSAGE);
+        }
     }
 
 }
